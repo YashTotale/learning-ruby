fa8461975fd23aa9b40fb1f4bde7930f7b78e346
Yash Totale <totaleyash@gmail.com>
2021-07-21 11:49:25 -0700

    Add Symbols File

---
+++ basics/objects/symbols.rb
@@ -0,0 +1,14 @@
+person = {
+  :first_name => 'Benjamin',
+  :last_name => 'Franklin'
+}
+
+# Methods
+person[:first_name]         # -> 'Benjamin'
+person[:first_name] = 'Tom' # first_name = 'Tom'
+
+# Shorthand
+book = {
+  title: 'Gone with the Wind',
+  rating: 5
+}
