84b93e22a9b98bcf0e06d4c45b1af52865d123fb
Yash Totale <totaleyash@gmail.com>
2021-07-21 13:09:47 -0700

    Add Exiting Script

---
+++ basics/scripting/exiting.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+fruits = ['banana', 'apple', 'pear']
+
+fruits.each do |fruit|
+  if fruit == 'apple'
+    # exit
+    # exit!
+    abort("Exit on apple")
+  end
+
+  puts fruit.capitalize
+end
+
+puts "Total fruits: #{fruits.count}"
