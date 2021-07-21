ac294982b143f867ba5dd5e35528814436a65108
Yash Totale <totaleyash@gmail.com>
2021-07-21 10:30:18 -0700

    Add Strings File

---
+++ basics/objects/strings.rb
@@ -0,0 +1,11 @@
+x = 'Hello'
+y = 'World'
+z = '1'
+
+# Methods
+x + ' ' + y   # -> 'Hello World'
+x * 3         # -> 'HelloHelloHello'
+y.reverse     # -> 'dlroW'
+z.length      # -> 1
+z.to_i        # -> 1
+x << ' World' # Changes x to 'Hello World'
