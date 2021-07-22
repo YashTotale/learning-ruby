01985e4243ef162db4fb2d240d861431fe4b064f
Yash Totale <totaleyash@gmail.com>
2021-07-22 12:31:36 -0700

    Add Return Value File

---
+++ basics/methods/return_value.rb
@@ -0,0 +1,29 @@
+def subtract(n1, n2)
+  n1 - n2
+end
+
+puts subtract(8, 3) # -> 5
+
+def positive_subtract(n1, n2)
+  result = n1 - n2
+  result = 0 if result < 0
+end
+
+puts positive_subtract(8, 3) # -> nil
+
+def better_positive_subtract(n1, n2)
+  result = n1 - n2
+  result = 0 if result < 0
+  result
+end
+
+puts better_positive_subtract(8, 3) # -> 5
+
+def longest_word(words = [])
+  longest = words.inject do |memo, word|
+    memo.length > word.length ? memo : word
+  end
+end
+
+fruits = ['apple', 'banana', 'pear']
+puts longest_word(fruits)
